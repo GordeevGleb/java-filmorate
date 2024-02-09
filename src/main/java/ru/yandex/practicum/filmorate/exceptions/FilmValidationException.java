957ba9946345b9c4e1df57246cbb93fcb80c07cf
Gleb <excexcexc@yandex.com>
2024-02-09 11:12:57 +0300

    Задание 9, Проверка гит-чекером 7

--- src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/FilmValidationException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class FilmValidationException extends Exception{
+public class FilmValidationException extends Exception {
     public FilmValidationException(String message) {
         super(message);
     }
--- src/main/java/ru/yandex/practicum/filmorate/exceptions/UserValidationException.java
+++ src/main/java/ru/yandex/practicum/filmorate/exceptions/UserValidationException.java
@@ -1,6 +1,6 @@
 package ru.yandex.practicum.filmorate.exceptions;
 
-public class UserValidationException extends Exception{
+public class UserValidationException extends Exception {
     public UserValidationException(String message) {
         super(message);
     }
