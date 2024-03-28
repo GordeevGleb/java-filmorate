aec84fa4cc662595b9c539060e4bbca0a6a4dba2
Gleb <excexcexc@yandex.com>
2024-03-28 17:42:49 +0300

    Задание 11

--- src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/MpaController.java
@@ -16,21 +16,23 @@ import java.util.List;
 @Slf4j
 public class MpaController {
     private final MpaService mpaService;
-@Autowired
+
+    @Autowired
     public MpaController(MpaService mpaService) {
         this.mpaService = mpaService;
     }
+
     @GetMapping("/mpa")
     public List<Mpa> getAll() {
         return mpaService.getAllMpa();
     }
+
     @GetMapping("/mpa/{mpaId}")
     public Mpa getById(@PathVariable int mpaId) throws MpaNotFoundException {
-    try {
-        return mpaService.getMpaById(mpaId).get();
-    }
-    catch (EmptyResultDataAccessException e) {
-        throw new MpaNotFoundException("Рейтинг с указанным в запросе id не найден");
-    }
+        try {
+            return mpaService.getMpaById(mpaId).get();
+        } catch (EmptyResultDataAccessException e) {
+            throw new MpaNotFoundException("Рейтинг с указанным в запросе id не найден");
+        }
     }
 }
