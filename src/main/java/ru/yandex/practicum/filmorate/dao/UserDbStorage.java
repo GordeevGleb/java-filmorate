40ea6e4b2421765fc461b28ec9a6fed5811f44d4
Gleb <excexcexc@yandex.com>
2024-03-29 23:59:14 +0300

    Задание 11

--- src/main/java/ru/yandex/practicum/filmorate/dao/UserDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/UserDbStorage.java
@@ -25,16 +25,16 @@ public class UserDbStorage implements UserStorage, RowMapper<User>, FriendServic
 
     private final JdbcTemplate jdbcTemplate;
 
-public User addUser(User user) {
-    SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
-            .withTableName("USERS")
-            .usingGeneratedKeyColumns("USER_ID");
-    if (user.getName().length() < 1) {
-        user.setName(user.getLogin());
+    public User addUser(User user) {
+        SimpleJdbcInsert simpleJdbcInsert = new SimpleJdbcInsert(jdbcTemplate)
+                .withTableName("USERS")
+                .usingGeneratedKeyColumns("USER_ID");
+        if (user.getName().length() < 1) {
+            user.setName(user.getLogin());
+        }
+        user.setId(simpleJdbcInsert.executeAndReturnKey(user.toMap()).longValue());
+        return user;
     }
-    user.setId(simpleJdbcInsert.executeAndReturnKey(user.toMap()).longValue());
-    return user;
-}
 
     @Override
     public User deleteUser(long userId) {
@@ -66,58 +66,59 @@ public User addUser(User user) {
 
     @Override
     public Optional<User> getUserById(long userId) throws UserNotFoundException {
-    try {
-        String sqlQuery = "select USER_ID, USER_EMAIL, USER_LOGIN, USER_NAME, USER_BIRTHDAY" +
-                " from USERS where USER_ID = ?";
-        return Optional.of(jdbcTemplate.queryForObject(sqlQuery, this::mapRow, userId));
-    } catch (EmptyResultDataAccessException e) {
-        throw new UserNotFoundException("Пользователь не найден");
+        try {
+            String sqlQuery = "select USER_ID, USER_EMAIL, USER_LOGIN, USER_NAME, USER_BIRTHDAY" +
+                    " from USERS where USER_ID = ?";
+            return Optional.of(jdbcTemplate.queryForObject(sqlQuery, this::mapRow, userId));
+        } catch (EmptyResultDataAccessException e) {
+            throw new UserNotFoundException("Пользователь не найден");
+        }
     }
-    }
+
     @Override
     public void addFriend(Long userId, Long friendId) throws UserNotFoundException {
-    try {
-        String sqlQuery = "insert into FRIENDSHIPS(USER_ID, FRIEND_ID) VALUES(?, ?)";
-        jdbcTemplate.update(sqlQuery, userId, friendId);
-    } catch (EmptyResultDataAccessException e) {
-        throw new UserNotFoundException("Пользователь не найден");
-    }
+        try {
+            String sqlQuery = "insert into FRIENDSHIPS(USER_ID, FRIEND_ID) VALUES(?, ?)";
+            jdbcTemplate.update(sqlQuery, userId, friendId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new UserNotFoundException("Пользователь не найден");
+        }
     }
 
     @Override
     public List<User> getFriends(Long userId) throws UserNotFoundException {
-    try {
-        String test = "select u.*" +
-                " from FRIENDSHIPS AS fs" +
-                " left join USERS as U on U.USER_ID = fs.FRIEND_ID" +
-                " where fs.USER_ID = ?";
-        return jdbcTemplate.query(test, this::mapRow, userId);
-    } catch (EmptyResultDataAccessException e) {
-        throw new UserNotFoundException("Пользователь не найден");
-    }
+        try {
+            String test = "select u.*" +
+                    " from FRIENDSHIPS AS fs" +
+                    " left join USERS as U on U.USER_ID = fs.FRIEND_ID" +
+                    " where fs.USER_ID = ?";
+            return jdbcTemplate.query(test, this::mapRow, userId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new UserNotFoundException("Пользователь не найден");
+        }
     }
 
     @Override
     public void deleteFriend(Long userId, Long friendId) throws UserNotFoundException {
-    try {
-        String sqlQuery = "delete from FRIENDSHIPS where USER_ID = ? AND FRIEND_ID = ?";
-        jdbcTemplate.update(sqlQuery, userId, friendId);
-    } catch (EmptyResultDataAccessException e) {
-        throw new UserNotFoundException("Пользователь не найден");
-    }
+        try {
+            String sqlQuery = "delete from FRIENDSHIPS where USER_ID = ? AND FRIEND_ID = ?";
+            jdbcTemplate.update(sqlQuery, userId, friendId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new UserNotFoundException("Пользователь не найден");
+        }
     }
 
     @Override
     public List<User> getMutualFriends(Long userId1, Long userId2) throws UserNotFoundException {
-    try {
-        String sqlQuery = "select u.* from FRIENDSHIPS as f1" +
-                " inner join FRIENDSHIPS as f2 on f1.FRIEND_ID = f2.FRIEND_ID " +
-                "and f1.USER_ID = ? and f2.USER_ID = ? " +
-                "inner join USERS as u on u.USER_ID = f2.FRIEND_ID";
-        return jdbcTemplate.query(sqlQuery, this::mapRow, userId1, userId2);
-    } catch (EmptyResultDataAccessException e) {
-        throw new UserNotFoundException("Пользователь не найден");
-    }
+        try {
+            String sqlQuery = "select u.* from FRIENDSHIPS as f1" +
+                    " inner join FRIENDSHIPS as f2 on f1.FRIEND_ID = f2.FRIEND_ID " +
+                    "and f1.USER_ID = ? and f2.USER_ID = ? " +
+                    "inner join USERS as u on u.USER_ID = f2.FRIEND_ID";
+            return jdbcTemplate.query(sqlQuery, this::mapRow, userId1, userId2);
+        } catch (EmptyResultDataAccessException e) {
+            throw new UserNotFoundException("Пользователь не найден");
+        }
     }
 
     @Override
