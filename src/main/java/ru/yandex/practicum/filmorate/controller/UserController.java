9dc5a4c4720537176f04f634cbb7d8f93e9a870a
Gleb <excexcexc@yandex.com>
2024-03-30 00:15:32 +0300

    Задание 11

--- src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -23,30 +23,42 @@ public class UserController {
 
     @PostMapping("/users")
     public User addUser(@Valid @RequestBody User user) {
+        log.info("Добавление пользователя " + user.getName());
         userService.addUser(user);
+        log.info("Пользователь добавлен");
         return user;
     }
 
     @PutMapping("/users")
     public User updateUser(@Valid @RequestBody User user) throws UserNotFoundException {
+        log.info("Обновление данных пользователя " + user.getName());
         userService.updateUser(user);
+        log.info("Данные пользователя обновлены");
         return user;
     }
 
     @GetMapping("/users")
     public List<User> getAllUsers() {
-        return userService.getAllUsers();
+        log.info("Запрос на получение списка всех пользователей");
+        List<User> users = userService.getAllUsers();
+        log.info("Список пользователей получен клиентом");
+        return users;
     }
 
     @DeleteMapping("/users")
     public User deleteUser(long userId) {
+        log.info("Удаление пользователя с id " + userId);
         User userToDelete = userService.deleteUser(userId);
+        log.info("Пользователь удален");
         return userToDelete;
     }
 
     @GetMapping("users/{userId}")
     public User getUserById(@PathVariable long userId) throws UserNotFoundException {
-        return userService.getUserById(userId);
+        log.info("Запрос на получение пользователя с id " + userId);
+        User user = userService.getUserById(userId);
+        log.info("Пользователь с указанным id получен клиентом");
+        return user;
     }
 
     @PutMapping("/users/{userId}/friends/{friendId}")
@@ -63,7 +75,9 @@ public class UserController {
     @ResponseStatus(HttpStatus.OK)
     public void deleteFriend(@PathVariable Long userId,
                                    @PathVariable Long friendId) throws UserNotFoundException {
+        log.info("Запрос на удаление друга " + friendId + " пользователем " + userId);
         userService.deleteFriend(userId, friendId);
+        log.info("Пользователь " +friendId + " удален из списка друзей " + userId);
     }
 
     @GetMapping("/users/{userId}/friends")
@@ -77,6 +91,9 @@ public class UserController {
     @GetMapping("/users/{userId}/friends/common/{friendId}")
     public List<User> getMutualFriends(@PathVariable Long userId,
                                        @PathVariable Long friendId) throws UserNotFoundException {
-        return userService.getMutualFriends(userId, friendId);
+        log.info("Запрос на получение списка общих друзей пользователей " + userId + " и " + friendId);
+        List<User> userList = userService.getMutualFriends(userId, friendId);
+        log.info("Список общих друзей пользователей " + userId + " и " + friendId + " получен клиентом");
+        return userList;
     }
 }
