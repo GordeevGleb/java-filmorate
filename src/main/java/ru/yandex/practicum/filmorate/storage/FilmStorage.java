84c2d1e27a4f25bfa3570952a67c5c26debdac95
shmelactimel <135132888+shmelactimel@users.noreply.github.com>
2024-04-13 11:55:26 +0300

    Update FilmStorage.java

--- src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/FilmStorage.java
@@ -24,7 +24,7 @@ public interface FilmStorage {
     List<Film> getPopular(int count);
 
     List<Film> getByDirectorId(Long id, String sortBy);
-  
+
     List<Film> getFilmRecommendation(Long userId, Long userWithSimilarLikesId);
 
     List<Film> findByTitleContaining(String query);
